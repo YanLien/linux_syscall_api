c6ef3ed9ad6f97f3a32a1e63ddb604da2d45cf77
Azure-stars <Azure_stars@126.com>
2024-07-07 15:49:38 +0000

    Deploying to gh-pages from @ Starry-OS/linux_syscall_api@a5d88f93a71720795167e92c425e599292601df9 🚀

---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["linux_syscall_api",["",[["syscall_fs",[["ctype",[],["dir.rs","epoll.rs","eventfd.rs","file.rs","mod.rs","mount.rs","pidfd.rs","pipe.rs"]],["imp",[],["ctl.rs","epoll.rs","eventfd.rs","io.rs","link.rs","mod.rs","mount.rs","poll.rs","stat.rs"]]],["fs_syscall_id.rs","mod.rs"]],["syscall_mem",[],["imp.rs","mem_syscall_id.rs","mod.rs"]],["syscall_net",[],["imp.rs","mod.rs","net_syscall_id.rs","socket.rs"]],["syscall_task",[["imp",[],["futex.rs","mod.rs","schedule.rs","signal.rs","task.rs","utils.rs"]]],["mod.rs","task_syscall_id.rs"]]],["api.rs","ctypes.rs","lib.rs","syscall.rs","trap.rs"]]]\
+]'));
+createSrcSidebar();
